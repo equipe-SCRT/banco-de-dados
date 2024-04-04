8d8d1c0c0da840739b89128196b0d54f370bfe17
gabrielBmoraes <gabriel.moraes@sptech.school>
2024-04-03 21:14:23 -0300

    gb/fix/modelagem

--- tfg.sql
+++ tfg.sql
@@ -3,22 +3,22 @@ use tech_for_good;
 
 -- drop database tech_for_good;
 
-create table endereco(
-idendereco int primary key,
+create table if not exists endereco(
+id_endereco int primary key,
 logradouro varchar(45),
 bairro varchar(45),
 numero varchar(45),
-cep varchar(45),
+cep char(8),
 complemento varchar(45)
 );
 
-create table rota(
-idrota int primary key,
+create table if not exists rota(
+id_rota int primary key,
 criador_rota varchar(45),
 data_criacao varchar(45)
 );
 
-create table enderecos_rota(
+create table if not exists enderecos_rota(
 rota_idrota int,
 endereco_idendereco int,
 primary key(rota_idrota, endereco_idendereco),
@@ -26,12 +26,12 @@ constraint foreign key(rota_idrota) references rota(idrota),
 constraint foreign key(endereco_idendereco) references endereco(idendereco)
 );
 
-create table cesta(
-idcesta int primary key,
+create table if not exists cesta(
+id_cesta int primary key,
 nome_cesta varchar(45)
 );
 
-create table cesta_rota(
+create table if not exists cesta_rota(
 idrota int,
 idcesta int,
 primary key(idrota, idcesta),
@@ -39,19 +39,19 @@ constraint foreign key(idrota) references rota(idrota),
 constraint foreign key(idcesta) references cesta(idcesta)
 );
 
-create table produto_cesta(
+create table  if not exists produto_cesta(
 idproduto int primary key,
 produto_cesta varchar(45),
 fk_cesta int,
 foreign key (fk_cesta) references cesta(idcesta)
 );
 
-create table tipo_produto(
+create table  if not exists tipo_produto(
 idtipo_produto int primary key,
 nome_tipo_produto varchar(45)
 );
 
-create table produto(
+create table if not exists produto(
 idproduto int primary key,
 nome_produto varchar(45),
 data_validade date,
@@ -64,12 +64,12 @@ fktipo_produto int,
 foreign key (fktipo_produto) references tipo_produto(idtipo_produto)
 );
 
-create table estoque(
+create table  if not exists estoque(
 idestoque int primary key,
 nome varchar(45)
 );
 
-create table produto_estoque(
+create table if not exists produto_estoque(
 estoque_idestoque int,
 produto_idproduto int,
 primary key(estoque_idestoque, produto_idproduto),
@@ -77,7 +77,7 @@ constraint foreign key(estoque_idestoque) references estoque(idestoque),
 constraint foreign key(produto_idproduto) references produto(idproduto)
 );
 
-create table usuario(
+create table if not exists usuario(
 idusuario int primary key,
 nome varchar(45),
 email varchar(45),
@@ -88,12 +88,12 @@ fkestoque int,
 foreign key (fkestoque) references estoque(idestoque)
 );
 
-create table tipo_meta(
+create table if not exists tipo_meta(
 idtipo_meta int primary key,
 nome_tipo_meta varchar(45)
 );
 
-create table meta(
+create table if not exists meta(
 idmeta int,
 nome_meta varchar(45),
 valor_meta decimal(10, 2),
