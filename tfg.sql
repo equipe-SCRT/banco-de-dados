a6c9f0bc4f3c1a3091ff179f1703e7112ca3ec71
brenodelmondes <breno.delmondes@sptech.school>
2024-04-05 18:58:03 -0300

    bd/docs/script-atualizado/

--- tfg.sql
+++ tfg.sql
@@ -1,105 +1,122 @@
-create database tech_for_good;
-use tech_for_good;
+CREATE DATABASE tech_for_good;
+USE tech_for_good;
 
--- drop database tech_for_good;
+-- DROP DATABASE tech_for_good;
 
-create table if not exists endereco(
-id_endereco int primary key,
-logradouro varchar(45),
-bairro varchar(45),
-numero varchar(45),
-cep char(8),
-complemento varchar(45)
+CREATE TABLE IF NOT EXISTS rota (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45)
 );
 
-create table if not exists rota(
-id_rota int primary key,
-criador_rota varchar(45),
-data_criacao varchar(45)
+CREATE TABLE IF NOT EXISTS lote (
+    id INT PRIMARY KEY AUTO_INCREMENT
 );
 
-create table if not exists enderecos_rota(
-rota_idrota int,
-endereco_idendereco int,
-primary key(rota_idrota, endereco_idendereco),
-constraint foreign key(rota_idrota) references rota(idrota),
-constraint foreign key(endereco_idendereco) references endereco(idendereco)
+CREATE TABLE IF NOT EXISTS historico_rota (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    km_rodados VARCHAR(45),
+    qtd_colaboradores INT,
+    data_historico_rota DATE,
+    hora_inicio TIME, 
+    hora_fim TIME,
+    qtd_cestas INT,
+    fk_rota INT, 
+    fk_lote INT,
+    CONSTRAINT FOREIGN KEY(fk_rota) REFERENCES rota(id),
+    CONSTRAINT FOREIGN KEY(fk_lote) REFERENCES lote(id)
 );
 
-create table if not exists cesta(
-id_cesta int primary key,
-nome_cesta varchar(45)
+CREATE TABLE IF NOT EXISTS tipo_cesta (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45)
 );
 
-create table if not exists cesta_rota(
-idrota int,
-idcesta int,
-primary key(idrota, idcesta),
-constraint foreign key(idrota) references rota(idrota),
-constraint foreign key(idcesta) references cesta(idcesta)
+CREATE TABLE IF NOT EXISTS tipo_produto (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45)
 );
 
-create table  if not exists produto_cesta(
-idproduto int primary key,
-produto_cesta varchar(45),
-fk_cesta int,
-foreign key (fk_cesta) references cesta(idcesta)
+CREATE TABLE IF NOT EXISTS rua (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
+    fk_rota INT,
+    CONSTRAINT FOREIGN KEY(fkrota) REFERENCES rota(id)
 );
 
-create table  if not exists tipo_produto(
-idtipo_produto int primary key,
-nome_tipo_produto varchar(45)
+CREATE TABLE IF NOT EXISTS endereco (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    logradouro VARCHAR(45),
+    bairro VARCHAR(45),
+    numero VARCHAR(45),
+    cep CHAR(8),
+    fk_rua INT,
+    CONSTRAINT FOREIGN KEY(fk_rua) REFERENCES rua(id)
 );
 
-create table if not exists produto(
-idproduto int primary key,
-nome_produto varchar(45),
-data_validade date,
-peso decimal(10, 2),
-codigo_barras varchar(45),
-data_adicionado varchar(45),
-preco decimal(10, 2),
-marca varchar(45),
-fktipo_produto int,
-foreign key (fktipo_produto) references tipo_produto(idtipo_produto)
+CREATE TABLE IF NOT EXISTS condominio (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
+    fk_endereco INT,
+    CONSTRAINT FOREIGN KEY(fk_endereco) REFERENCES rua(id)
 );
 
-create table  if not exists estoque(
-idestoque int primary key,
-nome varchar(45)
+CREATE TABLE IF NOT EXISTS campanha (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
+    local VARCHAR(45),
+    data_campanha DATE,
+    qtdArrecadada INT
 );
 
-create table if not exists produto_estoque(
-estoque_idestoque int,
-produto_idproduto int,
-primary key(estoque_idestoque, produto_idproduto),
-constraint foreign key(estoque_idestoque) references estoque(idestoque),
-constraint foreign key(produto_idproduto) references produto(idproduto)
+CREATE TABLE IF NOT EXISTS origem (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    auta_de_sousa_rua INT,
+    itapora INT,
+    fk_condominio INT,
+    fk_campanha INT,
+    FOREIGN KEY (fk_condominio) REFERENCES condominio(id),
+    FOREIGN KEY (fk_campanha) REFERENCES campanha(id)
 );
 
-create table if not exists usuario(
-idusuario int primary key,
-nome varchar(45),
-email varchar(45),
-senha varchar(45),
-cpf char(11),
-tipo_usuario varchar(45),
-fkestoque int,
-foreign key (fkestoque) references estoque(idestoque)
+CREATE TABLE IF NOT EXISTS produto (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
+    data_validade DATE,
+    peso DECIMAL(10, 2),
+    fk_origem INT,
+    FOREIGN KEY (fk_origem) REFERENCES origem(id)
 );
 
-create table if not exists tipo_meta(
-idtipo_meta int primary key,
-nome_tipo_meta varchar(45)
+CREATE TABLE IF NOT EXISTS produto_cesta (
+    fk_produto INT,
+    fk_tipo_cesta INT,
+    PRIMARY KEY(fk_produto, fk_tipo_cesta),
+    FOREIGN KEY (fk_produto) REFERENCES produto(id),
+    FOREIGN KEY (fk_tipo_cesta) REFERENCES tipo_cesta(id)
 );
 
-create table if not exists meta(
-idmeta int,
-nome_meta varchar(45),
-valor_meta decimal(10, 2),
-idtipo_meta int,
-fkusuario int,
-primary key(idmeta, idtipo_meta),
-foreign key(idtipo_meta) references tipo_meta(idtipo_meta),
-foreign key(fkusuario) references usuario(idusuario)
+CREATE TABLE IF NOT EXISTS cesta (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    data_montagem DATE,
+    fk_historico_lote INT,
+    fk_lote INT,
+    fk_tipo_cesta INT,
+    CONSTRAINT FOREIGN KEY(fk_historico_lote) REFERENCES historico_rota(id),
+    CONSTRAINT FOREIGN KEY(fk_lote) REFERENCES lote(id),
+    CONSTRAINT FOREIGN KEY(fk_tipo_cesta) REFERENCES tipo_cesta(id)
+);
+
+CREATE TABLE IF NOT EXISTS usuario (
+    idusuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
+    email VARCHAR(45),
+    senha VARCHAR(45),
+    tipo_usuario INT
+);
+
+CREATE TABLE IF NOT EXISTS historico_mudanca (
+    id INT,
+    fk_usuario INT,
+    data_hora DATETIME,
+    FOREIGN KEY (data_hora) REFERENCES usuario(id)
 );
